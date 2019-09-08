8ab5fe8de6b8d12a1334039a49c552d0070f69ba
LAPTOP-C403633C\Dika Handika <dika.csui@gmail.com>
2019-09-09 02:37:06 +0700

    deploy

---
+++ precache-manifest.5a4d2f3149c59790f701541b56c64fab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b0f66680e774e40ebd9a4f364ea7d17",
+    "url": "/furniture-finder/style.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/furniture-finder/robots.txt"
+  },
+  {
+    "revision": "201c452e2d259d7b62d0",
+    "url": "/furniture-finder/js/chunk-vendors.34c1f61e.js"
+  },
+  {
+    "revision": "985820ae00839fc7d3ad",
+    "url": "/furniture-finder/js/app.11272c4d.js"
+  },
+  {
+    "revision": "e40ba3e0b62445c7f9e3a6f7e3dbce21",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "a07be921d0d9e7eae5bf0d66dd613d50",
+    "url": "/furniture-finder/img/search.a07be921.svg"
+  },
+  {
+    "revision": "201c452e2d259d7b62d0",
+    "url": "/furniture-finder/css/chunk-vendors.96a655cf.css"
+  },
+  {
+    "revision": "985820ae00839fc7d3ad",
+    "url": "/furniture-finder/css/app.3e38c1bc.css"
+  }
+];
